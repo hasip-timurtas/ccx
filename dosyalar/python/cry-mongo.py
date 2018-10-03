c1c3cce180286cc95d5607413527025a189d6f1b
hasip_timurtas <hasip.timurtas@gmail.com>
2018-10-04 02:19:23 +0300

    deminkine orderBooksCount de eklendi

--- dosyalar/python/cry-mongo.py
+++ dosyalar/python/cry-mongo.py
@@ -139,6 +139,7 @@ def GetOrderBookGroup(d):
     orderBooksCount = orderBooks.count()
 
     if orderBooksCount < 4: # Eğer 4 dayıt yoksa false döndür
+      print(orderBooksCount)
       print('Db de yeterince kayıt yok. coin:', d['coin'])
       return False
 
