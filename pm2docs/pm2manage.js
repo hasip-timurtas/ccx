15955229eb4a70315e6900b15b7e893fbd730a86
hasip_timurtas <hasip.timurtas@gmail.com>
2018-10-11 00:57:18 +0300

    pm2 yönetim eklendi

---
+++ pm2docs/pm2manage.js
@@ -0,0 +1,31 @@
+var pm2     = require('pm2')
+
+var prcesNames = ['cry-db-depth', 'cry-mongo', 'cry-sell-kontrol']
+
+
+function Basla(){
+  pm2.connect(async (err) => {
+    if (err) throw err
+    pm2.restart(prcesNames[0], function() {});
+    await sleep(30)
+    pm2.restart(prcesNames[1], function() {});
+    pm2.restart(prcesNames[2], function() {});
+    /*
+    var list = pm2.list((err2, list) => {
+      
+    })
+    */
+  
+    console.log(pm2)
+  })
+}
+
+
+function sleep (saniye) {
+  return new Promise(resolve => setTimeout(resolve, saniye * 1000))
+}
+
+
+setInterval(() => {
+  Basla()
+}, 1000 * 60 * 60 ); // 1 saatte bir çalışır.
