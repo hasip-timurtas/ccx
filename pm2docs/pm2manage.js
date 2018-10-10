2368c5fb1c7edabbd0c459228d471fcfe5c9257a
hasip_timurtas <hasip.timurtas@gmail.com>
2018-10-11 01:34:55 +0300

    pm2 manage refactoring

--- pm2docs/pm2manage.js
+++ pm2docs/pm2manage.js
@@ -6,17 +6,11 @@ var prcesNames = ['cry-db-depth', 'cry-mongo', 'cry-sell-kontrol']
 function Basla(){
   pm2.connect(async (err) => {
     if (err) throw err
-    pm2.restart(prcesNames[0], function() {});
-    await sleep(30)
-    pm2.restart(prcesNames[1], function() {});
-    pm2.restart(prcesNames[2], function() {});
-    /*
-    var list = pm2.list((err2, list) => {
-      
-    })
-    */
-  
-    console.log(pm2)
+    for (const prces of prcesNames) {
+      pm2.restart(prces, function() {});
+      console.log(prces + ' Başladı.')
+    }
+    //var list = pm2.list((err2, list) => {})
   })
 }
 
