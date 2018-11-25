436db1810f9ce60ea861accdd3fef2b971e0d40b
hasip_timurtas <hasip.timurtas@gmail.com>
2018-11-25 22:47:48 +0300

    ref

--- dosyalar/altcoins-buy-sell.js
+++ dosyalar/altcoins-buy-sell.js
@@ -134,17 +134,17 @@ class WsMongo {
             const checkTamUygun = anaCoinLtc.ask.total >= this.ortak.limits[firstBase] && anaCoinBtc.bid.total >= this.ortak.limits[secondBase] // CHECK TAM UYGUN
             const checkTamUygun2 = coinBtc.ask.total >= this.ortak.limits[secondBase] && coinLtc.bid.total >= this.ortak.limits[firstBase] // CHECK TAM UYGUN
 
-            if(fark > 1 && checkTamUygun && checkTamUygun2){  // %1 den fazla fark varsa tamam.
-                this.FdbIslemleri(coin, anaCoinLtc, coinBtc, fark)
-                /*
+            if(fark > 1){  // %1 den fazla fark varsa tamam.
                 if(checkTamUygun && checkTamUygun2) {
+                    this.FdbIslemleri(coin, anaCoinLtc, coinBtc, fark)
+                    /*
                     this.ortak.db.ref(this.fdbRoot+"-uygunlar").child(coin).set({fark, coin, anaCoin, first: anaCoinLtc, second: anaCoinBtc, third: coinBtc, fourth: coinLtc})
                     console.log(`${anaCoin} coini > ${coin} coinine ${firstBase} > ${secondBase} ile çevirince fark: `+ fark)
+                    */
+                }else{
+                    const fdbName = anaCoinLtc.market.replace('/','-') + '--' + coinBtc.market.replace('/','-')
+                    this.FdbCoiniSil(anaCoin, fdbName)
                 }
-                */
-            }else{
-                const fdbName = anaCoinLtc.market.replace('/','-') + '--' + coinBtc.market.replace('/','-')
-                this.FdbCoiniSil(anaCoin, fdbName)
             }
         }
     }
