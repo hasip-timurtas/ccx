3a29870e5caaa9377295722098768d24c79cea81
hasip_timurtas <hasip.timurtas@gmail.com>
2018-12-01 14:10:35 +0300

    sell kontrol 2 saniye bekleme kaldırıldı

--- okex/sell-kontrol.js
+++ okex/sell-kontrol.js
@@ -51,7 +51,7 @@ class SellKontrol {
         const promise1 = this.BalanceIslemdeOlanlar(islemdeBalances, openOrders)
         const promise2 = this.BalanceAvilableOlanlar(availableBalances)
         await Promise.all([promise1, promise2]).catch(e=> console.log(e))
-        await this.ortak.sleep(2) // 2 saniye bekle
+        //await this.ortak.sleep(2) // 2 saniye bekle
     }
 
     async BalanceAvilableOlanlar(balances){
