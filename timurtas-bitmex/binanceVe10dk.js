5093cd874658fb81ed1173a6d24e31120f6f93eb
hasip_timurtas <hasip.timurtas@gmail.com>
2019-02-11 23:11:43 +0300

    fix

--- timurtas-bitmex/binanceVe10dk.js
+++ timurtas-bitmex/binanceVe10dk.js
@@ -50,9 +50,7 @@ class SellKontrol {
         this.StartWsData()
         await this.ortak.sleep(10)
         console.log('Web socket dataları hazır.');
-        return
         this.PositionKontrol()
-        
         this.OnDakika()
         this.BinanceBasla()
         
