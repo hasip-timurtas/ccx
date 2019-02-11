d6eca4aeb40dbe329c6a0ce98ba3991d7c1c61fa
hasip_timurtas <hasip.timurtas@gmail.com>
2019-02-12 02:00:20 +0300

    binance fark 2

--- timurtas-bitmex/binanceVe10dk.js
+++ timurtas-bitmex/binanceVe10dk.js
@@ -33,7 +33,7 @@ class SellKontrol {
         this.bitmexLastPrice = null
         this.bitmexPrice = null
         this.bitmexPriceList = []
-        this.binanceFark = 1
+        this.binanceFark = 2
         this.loglama = false
         this.lastOrderDate = new Date()
         this.sonIslemBeklemeSuresi = 5 // saniye
