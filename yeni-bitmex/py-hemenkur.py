dd6281428d8d37bbcba2879063945df44848849c
hasip_timurtas <hasip.timurtas@gmail.com>
2019-02-14 06:20:21 +0300

    thread activated

--- yeni-bitmex/py-hemenkur.py
+++ yeni-bitmex/py-hemenkur.py
@@ -34,19 +34,20 @@ def run():
     # Run forever
     while(ws.ws.sock.connected):
         orderBook = ws.market_depth()
+        '''
         if orderBook[0]["asks"][0][1] < 1000:
             order = client.Order.Order_new(symbol='XBTUSD', side="Sell", orderQty=AMOUNT, price=orderBook[0]["asks"][0][0]).result()
         
 
         if orderBook[0]["bids"][0][1] < 1000 :
             client.Order.Order_new(symbol='XBTUSD', side="Buy", orderQty=AMOUNT, price=orderBook[0]["bids"][0][0]).result()
-        
         '''
+        
         firstSell = orderBook[0]["asks"][0][0]
         firstBuy = orderBook[0]["bids"][0][0]
-        #_thread.start_new_thread( hemenOrderKur, () )
-        hemenOrderKur()
-        '''
+        t_thread.start_new_thread( hemenOrderKur, () )
+        #hemenOrderKur()
+        
 
 def hemenOrderKur():
     global oncekiBuy, oncekiSell, AMOUNT, client, firstBuy, firstSell
