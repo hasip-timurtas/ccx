d2ec792f47acbf0d4598b1566e9546aeef06755b
hasip_timurtas <hasip.timurtas@gmail.com>
2019-02-14 03:12:47 +0300

    refactoring

--- yeni-bitmex/py-hemenkur.py
+++ yeni-bitmex/py-hemenkur.py
@@ -3,17 +3,17 @@ import logging
 from time import sleep
 import bitmex
 import _thread
-
 client = bitmex.bitmex(test=False, api_key="WUi67Xl7EjE6A0iUq1RFVENw", api_secret="9alw1YOYGOlMrvW6N6AEC5ulmUl9ZKIP4a2RSdCQvs_xQCCn")
-AMOUNT = 10
-firstBuy = 0
-oncekiBuy = 0
 
-firstSell = 0
-oncekiSell = 0
 # Basic use of websocket.
 def run():
-    global firstBuy, firstSell
+    global client
+    AMOUNT = 10
+    firstBuy = 0
+    oncekiBuy = 0
+
+    firstSell = 0
+    oncekiSell = 0
     
     #logger = setup_logger()
 
@@ -28,45 +28,42 @@ def run():
         orderBook = ws.market_depth()
         firstSell = orderBook[0]["asks"][0][0]
         firstBuy = orderBook[0]["bids"][0][0]
-        _thread.start_new_thread( hemenOrderKur, () )
+        #_thread.start_new_thread( hemenOrderKur, () )
         #hemenOrderKur()
        # logger.info(orderBook)
-
-def hemenOrderKur():
-    global oncekiBuy, oncekiSell, AMOUNT, client, firstBuy, firstSell
-    if oncekiSell == 0:
-        oncekiSell = firstSell
-    elif firstSell < oncekiSell:
-        print('Sell Fiat Düştü')
-        tempOnceki = oncekiSell
-        oncekiSell = firstSell
-        # Oncekileri boz
-        #client.Order.Order_cancelAll().result()
-        #SELL KUR
-        order = client.Order.Order_new(symbol='XBTUSD', side="Sell", orderQty=AMOUNT, price=firstSell).result()
-        sonraOrderBoz(order['OrderId'])
-        print("Sell kuruldu. Önceki price: "+ str(tempOnceki)+", şimdiki price: "+ str(firstSell))
+        if oncekiSell == 0:
+            oncekiSell = firstSell
+        elif firstSell < oncekiSell:
+            print('Sell Fiat Düştü')
+            tempOnceki = oncekiSell
+            oncekiSell = firstSell
+            # Oncekileri boz
+            #client.Order.Order_cancelAll().result()
+            #SELL KUR
+            order = client.Order.Order_new(symbol='XBTUSD', side="Sell", orderQty=AMOUNT, price=firstSell).result()
+            print("Sell kuruldu. Önceki price: "+ str(tempOnceki)+", şimdiki price: "+ str(firstSell))
+            sonraOrderBoz(order['OrderId'])
+        else:
+            oncekiSell = firstSell
             
-    else:
-        oncekiSell = firstSell
-          
-    if oncekiBuy == 0:
-        oncekiBuy = firstBuy
-    elif firstBuy > oncekiBuy:
-        print('BUY Fiat ÇIKTI')
-        tempOnceki = oncekiBuy
-        oncekiBuy = firstBuy
-        #Oncekileri boz
-        #client.Order.Order_cancelAll().result()
-        #SELL KUR
-        order = client.Order.Order_new(symbol='XBTUSD', side="Buy", orderQty=AMOUNT, price=firstBuy).result()
-        sonraOrderBoz(order['OrderId'])
-        print("Buy kuruldu. Önceki price: "+str(tempOnceki)+", şimdiki price: "+ str(firstBuy))
-    else:
-        oncekiBuy = firstBuy
-    
+        if oncekiBuy == 0:
+            oncekiBuy = firstBuy
+        elif firstBuy > oncekiBuy:
+            print('BUY Fiat ÇIKTI')
+            tempOnceki = oncekiBuy
+            oncekiBuy = firstBuy
+            #Oncekileri boz
+            #client.Order.Order_cancelAll().result()
+            #SELL KUR
+            order = client.Order.Order_new(symbol='XBTUSD', side="Buy", orderQty=AMOUNT, price=firstBuy).result()
+            print("Buy kuruldu. Önceki price: "+str(tempOnceki)+", şimdiki price: "+ str(firstBuy))
+            sonraOrderBoz(order['OrderId'])
+        else:
+            oncekiBuy = firstBuy
+
 
 def sonraOrderBoz(orderId):
+    global client
     sleep(60)
     client.Order.Order_cancel(orderID=orderId).result()
 
@@ -80,19 +77,6 @@ def sonraOrderBoz(orderId):
         logger.info("Recent Trades: %s\n\n" % ws.recent_trades())
         sleep(10)
 '''
-
-def setup_logger():
-    # Prints logger info to terminal
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)  # Change this to DEBUG if you want a lot more info
-    ch = logging.StreamHandler()
-    # create formatter
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    # add formatter to ch
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
-    return logger
-
 
 if __name__ == "__main__":
     run()
