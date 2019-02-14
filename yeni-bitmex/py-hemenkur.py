f4cbb01126e897fb37993f37e8adcf2a0dc8ca68
hasip_timurtas <hasip.timurtas@gmail.com>
2019-02-14 05:42:23 +0300

    amount 1 test

--- yeni-bitmex/py-hemenkur.py
+++ yeni-bitmex/py-hemenkur.py
@@ -11,7 +11,7 @@ import bitmex
 import _thread
 
 client = bitmex.bitmex(test=False, api_key="WUi67Xl7EjE6A0iUq1RFVENw", api_secret="9alw1YOYGOlMrvW6N6AEC5ulmUl9ZKIP4a2RSdCQvs_xQCCn")
-AMOUNT = 10
+AMOUNT = 1
 firstBuy = 0
 oncekiBuy = 0
 
