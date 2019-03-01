58caa01bba84022c8c25146c35c34dba26b66974
hasip_timurtas <hasip.timurtas@gmail.com>
2019-03-01 22:41:37 +0300

    balance bitince başa al eklendi

--- pok/index.js
+++ pok/index.js
@@ -17,19 +17,30 @@ async function BetBaslat(){
 
     var seedReesult = await rp({jar:true, json:true, url: 'https://cashgames.bitcoin.com/roulette/reseed?account_key='+account_key}).catch(e=> console.log(e))
     var seedHash = seedReesult.server_seed_hash
-
+    var balance
     while(true){
+        if(balance && balance < baslangicBet){
+            console.log("Balance Bitti")
+            await sleep(10)
+            continue
+        }
+
         var randomNumber = Math.floor(Math.random() * 6)
         var game = games[randomNumber]
         var client_seed = crypto.randomBytes(32).toString('hex')
         options.url = `https://cashgames.bitcoin.com/roulette/spin?server_seed_hash=${seedHash}&client_seed=${client_seed}&progressive_bet=0&${game}=${bet}&use_fake_credits=true`
         var result = await rp(options).catch(e=> console.log(e))
+        balance = result.fake_intbalance
         seedHash = result.server_seed_hash
         if(result.intwinnings > 0){
             bet = baslangicBet
             console.log("YENDİ bet: ", bet)
         }else{
             bet = bet * 2
+            if(bet > balance){
+                bet = baslangicBet
+                console.log('Balance bitti, bet sıfırlanıyor. bet: ', bet);
+            }
             console.log("Kaybetti. bet: ", bet)
             /*
             if(bet / baslangicBet > 3){
@@ -48,12 +59,19 @@ async function BetBaslat(){
 async function AradaBirRandom(){
     var seedReesult = await rp({jar:true, json:true, url: 'https://cashgames.bitcoin.com/roulette/reseed?account_key='+account_key}).catch(e=> console.log(e))
     var seedHash = seedReesult.server_seed_hash
+    var balance
     while(true){
+        if(balance && balance < baslangicBet){
+            console.log("Balance Bitti")
+            await sleep(10)
+            continue
+        }
         var randomNumber = Math.floor(Math.random() * 6)
         var game = games[randomNumber]
         var client_seed = crypto.randomBytes(32).toString('hex')
         options.url = `https://cashgames.bitcoin.com/roulette/spin?server_seed_hash=${seedHash}&client_seed=${client_seed}&progressive_bet=0&${game}=${bet}&use_fake_credits=true`
         var result = await rp(options).catch(e=> console.log(e))
+        balance = result.fake_intbalance
         seedHash = result.server_seed_hash
         if(result.intwinnings > 0){
             console.log("Aradabir kazandı 1")
