89a8af3b4d2fbf2937502cc19ca5a48a9a3204fc
hasip_timurtas <hasip.timurtas@gmail.com>
2019-03-02 02:55:06 +0300

    balance log update

--- pok/index.js
+++ pok/index.js
@@ -36,7 +36,7 @@ async function BetBaslat(){
         var result = await rp(options).catch(e=> console.log(e))
         if(result.error== 'insufficient_funds') return console.log('Balance Bitti Çıkıyor.')
         balance = result.fake_intbalance
-        console.log("###################    BALANCE :  "+ result.fake_intbalance);
+        console.log("###################    BALANCE :  "+ result.fake_intbalance / 10000);
         
         seedHash = result.server_seed_hash
         if(result.intwinnings > 0){
