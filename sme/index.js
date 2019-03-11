6764809fe7b38e81a496ffb4b82d7395c4c0d9a4
hasip_timurtas <hasip.timurtas@gmail.com>
2019-03-11 14:42:23 -0300

    karinanın hastagları etkin

--- sme/index.js
+++ sme/index.js
@@ -3,32 +3,50 @@ const rp = require('request-promise')
 const randomWords = require('random-words')
 const FileCookieStore = require('tough-cookie-filestore2')
 const cookieStore = new FileCookieStore('./cookies.json')
-
+const kariHashTags = `
+#nycnewbornphotographer #nyctattoo #nycbrides #nycrealestate #nycbarber #nychotel #nycstore #nycbrand #nycrestaurants #nycbars #nycphotoshoot #nycnews #nycextensions #plasticsurgerynyc #nycsinger #nycfitness #nycpainters #nycdancers #nycweddings #nycweddingphotography
+#neworleansnails #delawarehairstylist #delawarebraider #ohiomakeupartist #ohiophotographer #georgiarealestate #ohiorealestate #nycrealestate #miamirealestate #miamibraider #washingtonrealestate #ukrealestate #ukbride #nycbrides #makeupturorial #beautybloggerlife #professionalorganizer #luxuryweddingplanner #weddingplannermalaysia #weddingplannermadrid #personaltrainerdubai
+#nycmodel #nycactress #newyorkinfluencers #miamiinfluencer #miamimodel #springseason #newarrivalsdaily #fashionphotographerdubai #dubaimodel #dubaiinfluencers #abudhabiinfluencer #dubailifestyle #dubaihorses #dubaiwriters #dubaiphotography #photographer_pics #nycphotographers #washington_ig #nycmodels
+#disoverysection #nychairdresser #makeupnyc #newjerseybraider #newjerseymodel #newjerseyfitness #ohiobarbers #minnesotabarbers #ohiofitness #ohiomakeupartist #arizonabarbers #arkansasfitness #coloradobarber #mainemodel #mainemakeupartist #massachusettsphotography #hampshiremakeupartist #michiganfitness #carolinabarber #nycbridalmarket
+`.replace(/\s/g, '').split('#')
+kariHashTags.shift()
 
 
 class IgAutoUpload {
     async Basla(){
+        let sayac = 1
         while(true){
             const client = new Instagram({ username: 'sosyal.bayii', password: 'karina3434+', cookieStore  })
             await client.login()
             const profile = await client.getProfile()
             console.log(profile.email);
-            
-            const hashTags = await this.GetHashTags()
+            const hashTags = this.GetRamdom30Tags() //await this.GetHashTags()
             const randomNumber = Math.floor(Math.random() * 16)  // 1-6 arası random sayı
             const photo = `http://keskinmedia.com/ig/${randomNumber}.jpg`
             const caption = 'DM US FOR MORE INFORMATION! ' + hashTags
             const result = await client.uploadPhoto({ photo, caption })
             if(result.status == "ok"){
-                console.log("Resim Yüklendi");
+                console.log(sayac + " defa resim Yüklendi")
+                sayac++
             }else{
-                console.log("Resim yüklerken hata oluştu. tekrar deneyecek");
+                console.log("Resim yüklerken hata oluştu. tekrar deneyecek")
             }
-            var a = 1
             await this.sleep(60 * 60 * 3) // 3 saatte bir 
         }
         
     }
+
+    GetRamdom30Tags(){
+        var arr = kariHashTags
+        var result = []
+        var _tmp = arr.slice()
+        for(var i = 0; i<30; i++){
+          var index = Math.ceil(Math.random() * 10) % _tmp.length
+          result.push(_tmp.splice(index, 1)[0])
+        }
+        return result
+    }
+
 
     async GetHashTags(){
         const word = randomWords()
